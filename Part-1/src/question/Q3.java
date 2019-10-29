9a82d5d4660d5c0b993abb17cf434b5279ce595f
张泽龙 <553234736@qq.com>
2019-10-29 09:03:15 +0800

    更新01-03代码；

--- Part-1/src/question/Q3.java
+++ Part-1/src/question/Q3.java
@@ -1,5 +1,6 @@
 package question;
 
+import org.junit.Test;
 import utils.ConsumeTimeFactory;
 
 import java.util.Arrays;
@@ -10,6 +11,57 @@ import java.util.stream.IntStream;
 
 public class Q3 {
     public static void main(String[] args) {
+        int size = 1000_0000;
+        List<Integer> initList = IntStream
+                .range(0, size)
+                .boxed()
+                .collect(Collectors.toList());
+        int[] array_int = new int[size];
+        int[] array_int_parallel = new int[size];
+        int[] array_int_bit_sort = new int[size];
+        new ConsumeTimeFactory()
+                // shuffle list
+                .before(() -> Collections.shuffle(initList))
+                .before(() -> {
+                    for (int i = 0; i < size; i++) {
+                        array_int[i] = initList.get(i);
+                        array_int_parallel[i] = initList.get(i);
+                        array_int_bit_sort[i] = initList.get(i);
+                    }
+                })
+                .start()
+                .end();
+        //todo 通过测试发现，在main函数仅有并行排序和位排序的情况下，先进行并行排序的时间性能反而极差，先进行位排序再进行并行排序时，并行排序时间正常。why？
+
+        // using parallel to compare
+        new ConsumeTimeFactory()
+                .start()
+                // sort
+                .doSth(() -> Arrays.parallelSort(array_int_parallel))
+                .end()
+                .print();
+        // bit sort
+        new ConsumeTimeFactory()
+                .start()
+                // sort
+                .doSth(() -> {
+                    for (int i = 0; i < array_int_bit_sort.length; i++) {
+                        Q2.BitSort.set(i);
+                    }
+                })
+                .end()
+                .print();
+        // using sort to compare
+        new ConsumeTimeFactory()
+                .start()
+                // sort
+                .doSth(() -> Arrays.sort(array_int))
+                .end()
+                .print();
+    }
+
+    @Test
+    public void bad_sort_order() {
         int size = 1000_0000;
         List<Integer> initList = IntStream
                 .range(0, size)
@@ -28,6 +80,15 @@ public class Q3 {
                 })
                 .start()
                 .end();
+        //todo 通过测试发现，在仅有并行排序和位排序的情况下，先进行并行排序的时间性能反而极差，先进行位排序再进行并行排序时，并行排序时间正常。why？
+
+        // using parallel to compare
+        new ConsumeTimeFactory()
+                .start()
+                // sort
+                .doSth(() -> Arrays.parallelSort(array_int_parallel))
+                .end()
+                .print();
         // bit sort
         new ConsumeTimeFactory()
                 .start()
@@ -37,13 +98,6 @@ public class Q3 {
                         Q2.BitSort.set(i);
                     }
                 })
-                .end()
-                .print();
-        // using parallel to compare
-        new ConsumeTimeFactory()
-                .start()
-                // sort
-                .doSth(() -> Arrays.parallelSort(array_int_parallel))
                 .end()
                 .print();
     }
