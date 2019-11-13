75f31a01be455bb89300e1b9b13f02a084c5176b
张泽龙 <553234736@qq.com>
2019-11-13 08:38:38 +0800

    更新02-C代码

---
+++ Part-2/src/question/QC.java
@@ -0,0 +1,28 @@
+package question;
+
+import java.util.*;
+
+/**
+ * @author zzl
+ */
+public class QC {
+    public static void main(String[] args) {
+//        变位词
+        List<String> inputWords = Arrays.asList("abc", "cba", "love", "evol", "no", "nothing", "");
+        Map<String, ArrayList<String>> labelWithWords = new HashMap<>();
+        char[] chars;
+        for (String word : inputWords) {
+            chars = word.toCharArray();
+            Arrays.sort(chars);
+            String newOrder = new String(chars);
+            if (labelWithWords.containsKey(newOrder)) {
+                labelWithWords.get(newOrder).add(word);
+            } else {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(word);
+                labelWithWords.put(newOrder, list);
+            }
+        }
+        System.out.println(labelWithWords);
+    }
+}
