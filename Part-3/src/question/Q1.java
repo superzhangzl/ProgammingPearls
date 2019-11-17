63c2c7093a95038c52aaa08367f359becf8fcca8
张泽龙 <553234736@qq.com>
2019-11-17 18:12:47 +0800

    add base file

---
+++ Part-3/src/question/Q1.java
@@ -0,0 +1,13 @@
+package question;
+
+import java.util.Arrays;
+
+/**
+ * @author zzl
+ */
+public class Q1 {
+    public static void main(String[] args) {
+        int[] arr_ordered = new int[100];
+        int result = Arrays.binarySearch(arr_ordered, 5);
+    }
+}
