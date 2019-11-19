78ed302b2bc13b168c74d5e5d6031e0a6445443a
张泽龙 <553234736@qq.com>
2019-11-19 09:41:17 +0800

    update 03-01,add print func

--- Part-3/src/question/Q1.java
+++ Part-3/src/question/Q1.java
@@ -11,7 +11,7 @@ public class Q1 {
     private static final BigDecimal startRange = BigDecimal.valueOf(2200.0d);
     private static final BigDecimal minPercent = BigDecimal.valueOf(0.14d);
     private static final BigDecimal basePercent = BigDecimal.valueOf(0.01d);
-    private static final BigDecimal maxPercent = BigDecimal.valueOf(0.180d);
+    private static final BigDecimal maxPercent = BigDecimal.valueOf(0.700d);
     private static final BigDecimal div = BigDecimal.valueOf(500d);
     // 新建税率表，然后直接根据表二分查找带入公式计算即可
     private static List<TaxQueen> table;
@@ -22,7 +22,7 @@ public class Q1 {
         table.add(min);
         TaxQueen current;
         int index = 1;
-        for (BigDecimal i = minPercent.add(basePercent); i.compareTo(maxPercent) != 0; i = i.add(basePercent)) {
+        for (BigDecimal i = minPercent.add(basePercent); i.compareTo(maxPercent) <= 0; i = i.add(basePercent)) {
             BigDecimal currentBasePercent = BigDecimal.valueOf(div.doubleValue() * table.get(index - 1).currentPercent.doubleValue() + table.get(index - 1).baseTax.doubleValue());
             current = new TaxQueen(BigDecimal.valueOf(table.get(index - 1).baseRange.doubleValue() + div.doubleValue()), currentBasePercent, i);
             table.add(current);
@@ -31,7 +31,14 @@ public class Q1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(table);
+        printTable(table);
+    }
+
+    public static void printTable(List<TaxQueen> table) {
+        table.forEach(tax -> {
+            String line = String.format("|%10.2f|%10.2f|%10.2f|", tax.baseRange.floatValue(), tax.baseTax.floatValue(), tax.currentPercent.floatValue());
+            System.out.println(line);
+        });
     }
 
 
